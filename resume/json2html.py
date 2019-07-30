da7f54b589ca1e6f7a3b20aa1738f4756f35de3c
Steve Johnson <steve@steveasleep.com>
2019-07-30 14:26:14 -0400

    Themes of three

--- resume/json2html.py
+++ resume/json2html.py
@@ -8,14 +8,33 @@ def main():
   with open('template.html', 'r') as f:
     template = f.read()
 
-  with open(sys.argv[1], 'r') as f:
+  total = int(sys.argv[1])
+  n = int(sys.argv[2])
+
+  with open(sys.argv[3], 'r') as f:
     data = json.loads(f.read())
 
-  with open(sys.argv[2], 'r') as f:
+  with open(sys.argv[4], 'r') as f:
     css = f.read()
 
-  with open(sys.argv[3], 'w') as f:
-    f.write(jinja2.Template(template).render(css=css, **data))
+  prev_url = None
+  next_url = None
+  if n > 1:
+    prev_url = 'resume{}.html'.format(n - 1)
+  if n < total:
+    next_url = 'resume{}.html'.format(n + 1)
+
+  if prev_url == 'resume1.html':
+    prev_url = 'resume.html'
+
+  with open(sys.argv[5], 'w') as f:
+    f.write(jinja2.Template(template).render(
+      css=css,
+      next_url=next_url,
+      prev_url=prev_url,
+      total=total,
+      n=n,
+      **data))
 
 if __name__ == '__main__':
   main()
